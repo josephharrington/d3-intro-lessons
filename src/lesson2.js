2c9da2b7338dfb0751fdf7f1137136d081b67a32
Joey Harrington <joseph@box.com>
2015-07-14 10:30:11 -0700

    Finish lesson 2

--- src/lesson2.js
+++ src/lesson2.js
@@ -5,13 +5,14 @@ var svg = d3.select('#content').append('svg')
 
 
 
-// Make 5 circles for us to play with.
+// Make 5 rectangles for us to play with.
 for (var i = 0; i < 5; i++) {
-    svg.append('circle').attr({
-        cx: 200 + i * 200,
-        cy: 300,
-        r: 50,
-        class: 'blueCircle'
+    svg.append('rect').attr({
+        x: 200 + i * 200,
+        y: 100,
+        width: 100,
+        height: 200,
+        class: 'blueShape'
     });
 }
 
@@ -19,12 +20,12 @@ for (var i = 0; i < 5; i++) {
 // Selections are one of the core concepts of d3. A selection is essentially an
 // array of DOM elements. But a selection is a special d3 object -- it's
 // actually a subclass of array with lots of extra methods.
-var circles = svg.selectAll('circle');
+var rectangles = svg.selectAll('rect');
 
 
 // Assigning properties to "window" makes them available in the js console.
-// Try inspecting the circles variable in the JS console.
-window.circles = circles;
+// Try inspecting the rects variable in the JS console.
+window.rectangles = rectangles;
 
 
 // See a description and list of all the selection methods:
@@ -34,14 +35,14 @@ window.circles = circles;
 // You can also provide a function when assigning attributes.
 // D3 will call the function to get a value for the attribute.
 window.randomize = function() {
-    circles.attr('r', function() {
-        return Math.random() * 100;
+    rectangles.attr('height', function() {
+        return Math.random() * 300;
     });
 };
 
 
-// You can "bind" data to a selection. Here, we'll bind ints to the circles.
-circles.data([1, 2, 3, 4, 5]);
+// You can "bind" data to a selection. Here, we'll bind ints to the rects.
+rectangles.data([1, 2, 3, 4, 5]);
 // Binding data doesn't do anything by itself. It actually just assigns each
 // value to the __data__ property of each element. (You can see this in the
 // Properties inspector in Chrome devtools.)
@@ -49,49 +50,45 @@ circles.data([1, 2, 3, 4, 5]);
 
 // If we use a function to assign an attribute, D3 passes the element's data
 // as the first argument.
-window.update1 = function() {
-    circles
-        .attr('r', function(datum) {
-            return datum * 10;
+window.updateHeight = function() {
+    rectangles
+        .attr('height', function(datum) {
+            return datum * 50;
         });
 };
 
 
 // Try running these in the console.
-//    circles.data([5, 4, 3, 2, 1])
-//    circles.attr('r', function(d) {return d * 10})
+//    rectangles.data([5, 4, 3, 2, 1])
+//    rectangles.attr('height', function(d) {return d * 50})
 
 
 
 // Setting data and updating attributes is often done at the same time.
-window.update2 = function(someNewData) {
-    circles
+window.updateHeightData = function(someNewData) {
+    rectangles
         .data(someNewData)
-        .attr('r', function(d) {
-            return d * 10;
+        .attr('height', function(d) {
+            return d * 50;
         });
 };
 
 
 // And we can easily make these data changes look nice. :)
-window.update3 = function(someNewData) {
-    circles
+window.updateHeightDataWithTransition = function(someNewData) {
+    rectangles
         .data(someNewData)
-        .transition(1000)
-        .attr('r', function(d) {
-            return d * 10;
+        .transition()
+//        .duration(2000)
+        .attr('height', function(d) {
+            return d * 50;
         });
 };
-//circles.data([1, 2, 3, 4, 5]).attr('r', function(d) {return d * 10});
-//circles.data([1, 5, 2, 2, 1]).transition(3000).attr('r', function(d) {return d * 10});
 
 
+// But what happens if your selection and data are different sizes?
+// Try:
+//  updateHeightData([1, 2])
+//  updateHeightData([1, 2, 3, 4, 5, 6, 7])
 
-
-
-
-
-
-
-
-// But what happens if your selection and data are different sizes?...
+// Neither of the above do what we want...
